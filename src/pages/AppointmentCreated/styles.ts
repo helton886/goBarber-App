b5b4d73dc5cb6cc695649bf9fd379d8c14fbd0ff
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2021-02-04 01:03:53 -0300

    feature: appointment created page

---
+++ src/pages/AppointmentCreated/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  padding: 0 24px;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 32px;
+  color: #f4ede8;
+  margin-top: 48px;
+  text-align: center;
+`;
+
+export const Description = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  color: #999591;
+  margin-top: 16px;
+`;
+
+export const OkButton = styled(RectButton)`
+  padding: 12px 24px;
+  background: #ff9000;
+  justify-content: center;
+  border-radius: 10px;
+  margin-top: 24px;
+  align-items: center;
+`;
+
+export const OkButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  color: #312e38;
+  font-size: 18px;
+`;
